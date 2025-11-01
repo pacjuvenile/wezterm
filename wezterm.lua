859676700bc4f192b214e28050281a0e400ad3a6
packjuvenile <3283890195@qq.com>
2025-11-02 00:15:43 +0800

    modified:   wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -18,8 +18,13 @@ M.default_gui_startup_args = { "start", "--position", "110,110" }
 M.initial_cols = 120
 M.initial_rows = 25
 M.window_decorations = "RESIZE"
+M.adjust_window_size_when_changing_font_size = false
+M.hide_tab_bar_if_only_one_tab = true
+M.show_new_tab_button_in_tab_bar = false
+M.use_fancy_tab_bar = false
 
 -- opacity
 M.window_background_opacity = 0.9
+M.text_background_opacity = 0.9
 
 return M
