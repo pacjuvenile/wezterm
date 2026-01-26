91835c66bd32087a6e432c6b4e42b04d8d45bd69
packjuvenile <3283890195@qq.com>
2026-01-26 16:45:47 +0800

    modified:   wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -11,6 +11,7 @@ M.line_height = 1.2
 
 -- color scheme
 M.color_scheme = "Catppuccin Macchiato" -- Catppuccin Latte/Frappe/Macchiato/Mocha
+-- M.color_scheme = "Astrodark (Gogh)" -- Catppuccin Latte/Frappe/Macchiato/Mocha
 
 -- appearance
 -- window
@@ -31,7 +32,7 @@ M.window_padding = {
 
 -- opacity
 M.window_background_opacity = 0.9
-M.text_background_opacity = 0.7
+M.text_background_opacity = 0.9
 
 -- keyboard
 M.keys = {
