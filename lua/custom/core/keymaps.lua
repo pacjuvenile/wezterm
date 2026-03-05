69f8e971adfc0d9d517799ae10f05e87763b83b2
packjuvenile <3283890195@qq.com>
2026-03-05 20:33:11 +0800

    modified:   lua/custom/core/keymaps.lua

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -3,23 +3,30 @@ Config.disable_default_key_bindings = true
 
 -- keymap
 Config.keys = {
+	-- 基础快捷键
+	-- 终端内粘贴
+	{
+		key = 'v',
+		mods = 'CTRL|SHIFT',
+		action = Wezterm.action.PasteFrom('Clipboard')
+	},
 	-- 重载配置
 	{
 		key = 'R',
 		mods = 'CTRL|ALT|SHIFT',
 		action = Wezterm.action.ReloadConfiguration
 	},
+	-- 调试信息
+	{
+		key = 'L',
+		mods = 'CTRL|ALT|SHIFT',
+		action = Wezterm.action.ShowDebugOverlay
+	},
 	-- 命令面板
 	{
 		key = 'P',
 		mods = 'CTRL|ALT|SHIFT',
 		action = Wezterm.action.ActivateCommandPalette
-	},
-	-- 终端内粘贴
-	{
-		key = 'v',
-		mods = 'CTRL|SHIFT',
-		action = Wezterm.action.PasteFrom('Clipboard')
 	},
 	-- 插入Unicode字符
 	{
@@ -44,185 +51,177 @@ Config.keys = {
 		action = Wezterm.action.ResetFontSize
 	},
 
-	-- 面板尺寸调整
+	-- 窗格快捷键
+	-- 新建/删除窗格
+	{
+		key = 'r',
+		mods = 'CTRL|ALT',
+		action = Wezterm.action_callback(function(window, pane)
+			local u = pane:get_current_working_dir()
+			local cwd = nil
+			if type(u) == "table" then
+				cwd = u.file_path or u.path
+			elseif type(u) == "string" then
+				cwd = (u:gsub("^file://[^/]*", ""))
+			end
+			Wezterm.log_info("u=" .. tostring(u) .. " cwd=" .. tostring(cwd))
+			window:perform_action(
+				Wezterm.action.SplitPane({
+					direction = "Right",
+					size = { Percent = 50 },
+					command = {
+						domain = "CurrentPaneDomain",
+						cwd = cwd,
+					},
+				}),
+				pane
+			)
+		end)
+	},
+	{
+		key = 'd',
+		mods = 'CTRL|ALT',
+		action = Wezterm.action.SplitVertical({ domain = 'CurrentPaneDomain' })
+	},
+	{
+		key = 'x',
+		mods = 'CTRL|ALT',
+		action = Wezterm.action.CloseCurrentPane { confirm = false }
+	},
+	-- 切换窗格焦点
 	{
 		key = 'h',
 		mods = 'CTRL|ALT',
-		action = Wezterm.action.AdjustPaneSize({ "Left", 1 })
+		action = Wezterm.action.ActivatePaneDirection('Left')
 	},
 	{
 		key = 'j',
 		mods = 'CTRL|ALT',
-		action = Wezterm.action.AdjustPaneSize({ "Down", 1 })
+		action = Wezterm.action.ActivatePaneDirection('Down')
 	},
 	{
 		key = 'k',
 		mods = 'CTRL|ALT',
-		action = Wezterm.action.AdjustPaneSize({ "Up", 1 })
+		action = Wezterm.action.ActivatePaneDirection('Up')
 	},
 	{
 		key = 'l',
 		mods = 'CTRL|ALT',
+		action = Wezterm.action.ActivatePaneDirection('Right')
+	},
+	{
+		key = 'a',
+		mods = 'CTRL|ALT',
+		action = Wezterm.action.PaneSelect {
+			alphabet = 'asdfghjkl',
+			mode = 'Activate'
+		}
+	},
+	-- 移动窗格位置
+	{
+		key = 's',
+		mods = 'CTRL|ALT',
+		action = Wezterm.action.PaneSelect {
+			alphabet = 'asdfghjkl',
+			mode = 'SwapWithActive'
+		}
+	},
+	-- 调整窗格尺寸
+	{
+		key = 'H',
+		mods = 'CTRL|ALT|SHIFT',
+		action = Wezterm.action.AdjustPaneSize({ "Left", 1 })
+	},
+	{
+		key = 'J',
+		mods = 'CTRL|ALT|SHIFT',
+		action = Wezterm.action.AdjustPaneSize({ "Down", 1 })
+	},
+	{
+		key = 'K',
+		mods = 'CTRL|ALT|SHIFT',
+		action = Wezterm.action.AdjustPaneSize({ "Up", 1 })
+	},
+	{
+		key = 'L',
+		mods = 'CTRL|ALT|SHIFT',
 		action = Wezterm.action.AdjustPaneSize({ "Right", 1 })
+	},
+	{
+		key = 'F',
+		mods = 'CTRL|ALT|SHIFT',
+		action = Wezterm.action.TogglePaneZoomState
+	},
+
+	-- 标签页快捷键
+	-- 标签页新建/删除
+	{
+		key = 't',
+		mods = 'CTRL|ALT',
+		action = Wezterm.action.SpawnTab('CurrentPaneDomain')
+	},
+	{
+		key = 'w',
+		mods = 'CTRL|ALT',
+		action = Wezterm.action.CloseCurrentTab({ confirm = false })
+	},
+	-- 标签页切换
+	{
+		key = 'p',
+		mods = 'CTRL|ALT',
+		action = Wezterm.action.ActivateTabRelative(-1)
+	},
+	{
+		key = 'n',
+		mods = 'CTRL|ALT',
+		action = Wezterm.action.ActivateTabRelative(1)
 	},
 	{
 		key = 'f',
 		mods = 'CTRL|ALT',
-		action = Wezterm.action.TogglePaneZoomState
+		action = Wezterm.action.ShowTabNavigator
+	},
+	-- 标签页移动
+	{
+		key = 'LeftArrow',
+		mods = 'CTRL|ALT',
+		action = Wezterm.action.MoveTabRelative(-1)
+	},
+	{
+		key = 'RightArrow',
+		mods = 'CTRL|ALT',
+		action = Wezterm.action.MoveTabRelative(1)
+	},
+	-- 标签页重命名
+	{
+		key = 'r',
+		mods = 'CTRL|ALT',
+		action = Wezterm.action.PromptInputLine {
+			description = 'Rename tab title',
+			action = Wezterm.action_callback(function(window, _, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		},
 	},
 
-	-- 进入复制模式
+	-- 检索快捷键
+	-- 快速复制
 	{
 		key = 'y',
 		mods = 'CTRL|ALT',
-		action = Wezterm.action.ActivateCopyMode
+		action = Wezterm.action.QuickSelect
 	},
-
-	-- 进入窗格模式
+	-- 进入搜索模式
 	{
-		key = 'p',
+		key = '/',
 		mods = 'CTRL|ALT',
-		action = Wezterm.action.ActivateKeyTable {
-			name = 'pane_mode',
-			one_shot = true,
-			until_unknown = true
-		}
-	},
-
-	-- 进入标签页模式
-	{
-		key = 't',
-		mods = 'CTRL|ALT',
-		action = Wezterm.action.ActivateKeyTable {
-			name = 'tab_mode',
-			one_shot = true,
-			until_unknown = true
-		}
+		action = Wezterm.action.Search({ CaseSensitiveString = "" })
 	},
 }
 
 Config.key_tables = {
-	-- 复制模式键位
-	copy_mode = {
-		-- quit
-		{
-			key = 'Escape',
-			mods = 'NONE',
-			action = Wezterm.action.Multiple {
-				-- Wezterm.action.CopyMode 'MoveToScrollbackBottom',
-				Wezterm.action.CopyMode 'Close'
-			}
-		},
-		-- navigation
-		{
-			key = 'h',
-			mods = "NONE",
-			action = Wezterm.action.CopyMode 'MoveLeft'
-		},
-		{
-			key = 'j',
-			mods = "NONE",
-			action = Wezterm.action.CopyMode 'MoveDown'
-		},
-		{
-			key = 'k',
-			mods = "NONE",
-			action = Wezterm.action.CopyMode 'MoveUp'
-		},
-		{
-			key = 'l',
-			mods = "NONE",
-			action = Wezterm.action.CopyMode 'MoveRight'
-		},
-		{
-			key = 'w',
-			mods = 'NONE',
-			action = Wezterm.action.CopyMode 'MoveForwardWord'
-		},
-		{
-			key = 'e',
-			mods = 'NONE',
-			action = Wezterm.action.CopyMode 'MoveForwardWordEnd'
-		},
-		{
-			key = 'b',
-			mods = 'NONE',
-			action = Wezterm.action.CopyMode 'MoveBackwardWord'
-		},
-		{
-			key = '0',
-			mods = 'NONE',
-			action = Wezterm.action.CopyMode 'MoveToStartOfLine'
-		},
-		{
-			key = '^',
-			mods = 'SHIFT',
-			action = Wezterm.action.CopyMode 'MoveToStartOfLineContent'
-		},
-		{
-			key = '$',
-			mods = 'SHIFT',
-			action = Wezterm.action.CopyMode 'MoveToEndOfLineContent'
-		},
-		{
-			key = "g",
-			mods = 'NONE',
-			action = Wezterm.action.CopyMode 'MoveToScrollbackTop'
-		},
-		{
-			key = 'G',
-			mods = 'SHIFT',
-			action = Wezterm.action.CopyMode 'MoveToScrollbackBottom'
-		},
-		{
-			key = 'd',
-			mods = 'CTRL',
-			action = Wezterm.action.CopyMode { MoveByPage = 0.5 }
-		},
-		{
-			key = 'u',
-			mods = 'CTRL',
-			action = Wezterm.action.CopyMode { MoveByPage = -0.5 }
-		},
-		-- selection
-		{
-			key = 'v',
-			mods = 'NONE',
-			action = Wezterm.action.CopyMode { SetSelectionMode = 'Cell' }
-		},
-		{
-			key = 'V',
-			mods = 'SHIFT',
-			action = Wezterm.action.CopyMode { SetSelectionMode = 'Line' }
-		},
-		{
-			key = 'v',
-			mods = 'CTRL',
-			action = Wezterm.action.CopyMode { SetSelectionMode = 'Block' }
-		},
-		-- yank
-		{
-			key = 'y',
-			mods = 'NONE',
-			action = Wezterm.action.Multiple {
-				Wezterm.action.CopyTo 'Clipboard',
-				Wezterm.action.CopyMode 'Close',
-			}
-		},
-		-- 进入选择模式
-		{
-			key = 'f',
-			mods = 'NONE',
-			action = Wezterm.action.QuickSelect
-		},
-		-- 进入搜索模式
-		{
-			key = '/',
-			mods = 'NONE',
-			action = Wezterm.action.Search({ CaseSensitiveString = "" })
-		},
-	},
-
 	-- 搜索模式键位
 	search_mode = {
 		{
@@ -249,132 +248,5 @@ Config.key_tables = {
 			mods = 'CTRL',
 			action = Wezterm.action.CopyMode 'CycleMatchType'
 		},
-	},
-
-	-- 面板模式键位
-	pane_mode = {
-		{
-			key = 'r',
-			mods = 'NONE',
-			action = Wezterm.action_callback(function(window, pane)
-				local u = pane:get_current_working_dir()
-				local cwd = nil
-				if type(u) == "table" then
-					cwd = u.file_path or u.path
-				elseif type(u) == "string" then
-					cwd = (u:gsub("^file://[^/]*", ""))
-				end
-
-				Wezterm.log_info("u=" .. tostring(u) .. " cwd=" .. tostring(cwd))
-				window:perform_action(
-					Wezterm.action.SplitPane({
-						direction = "Right",
-						size = { Percent = 50 },
-						command = {
-							domain = "CurrentPaneDomain",
-							cwd = cwd,
-						},
-					}),
-					pane
-				)
-			end)
-		},
-		{
-			key = 'd',
-			mods = 'NONE',
-			action = Wezterm.action.SplitVertical({ domain = 'CurrentPaneDomain' })
-		},
-		{
-			key = 'x',
-			mods = 'NONE',
-			action = Wezterm.action.CloseCurrentPane { confirm = false }
-		},
-		{
-			key = 'f',
-			mods = 'NONE',
-			action = Wezterm.action.PaneSelect {
-				alphabet = 'asdfghjkl',
-				mode = 'Activate'
-			}
-		},
-		{
-			key = 's',
-			mods = 'NONE',
-			action = Wezterm.action.PaneSelect {
-				alphabet = 'asdfghjkl',
-				mode = 'SwapWithActive'
-			}
-		},
-		{
-			key = 'h',
-			mods = 'NONE',
-			action = Wezterm.action.ActivatePaneDirection('Left')
-		},
-		{
-			key = 'j',
-			mods = 'NONE',
-			action = Wezterm.action.ActivatePaneDirection('Down')
-		},
-		{
-			key = 'k',
-			mods = 'NONE',
-			action = Wezterm.action.ActivatePaneDirection('Up')
-		},
-		{
-			key = 'l',
-			mods = 'NONE',
-			action = Wezterm.action.ActivatePaneDirection('Right')
-		},
-	},
-
-	-- 标签页模式键位
-	tab_mode = {
-		{
-			key = 't',
-			mods = 'NONE',
-			action = Wezterm.action.SpawnTab('CurrentPaneDomain')
-		},
-		{
-			key = 'w',
-			mods = 'NONE',
-			action = Wezterm.action.CloseCurrentTab({ confirm = false })
-		},
-		{
-			key = 'f',
-			mods = 'NONE',
-			action = Wezterm.action.ShowTabNavigator
-		},
-		{
-			key = 'p',
-			mods = 'NONE',
-			action = Wezterm.action.ActivateTabRelative(-1)
-		},
-		{
-			key = 'n',
-			mods = 'NONE',
-			action = Wezterm.action.ActivateTabRelative(1)
-		},
-		{
-			key = 'LeftArrow',
-			mods = 'NONE',
-			action = Wezterm.action.MoveTabRelative(-1)
-		},
-		{
-			key = 'RightArrow',
-			mods = 'NONE',
-			action = Wezterm.action.MoveTabRelative(1)
-		},
-		{
-			key = 'r',
-			mods = 'NONE',
-			action = Wezterm.action.PromptInputLine {
-				description = 'Rename tab title',
-				action = Wezterm.action_callback(function(window, _, line)
-					if line then
-						window:active_tab():set_title(line)
-					end
-				end),
-			},
-		}
 	}
 }
